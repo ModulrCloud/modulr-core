3ad915eeb924536beacd79d6fe2a90e101c275fc
VladChernenko <vladchernenko@protonmail.com>
2026-04-30 23:03:44 +0200

    Double pointer removed

--- databases/databases.go
+++ databases/databases.go
@@ -11,27 +11,27 @@ var BLOCKS, STATE, EPOCH_DATA, APPROVEMENT_THREAD_METADATA, FINALIZATION_THREAD_
 
 type namedDB struct {
 	name string
-	db   **leveldb.DB
+	db   *leveldb.DB
 }
 
 // CloseAll safely closes all initialized LevelDB instances
 func CloseAll() error {
 
 	databases := []namedDB{
-		{name: "BLOCKS", db: &BLOCKS},
-		{name: "STATE", db: &STATE},
-		{name: "EPOCH_DATA", db: &EPOCH_DATA},
-		{name: "APPROVEMENT_THREAD_METADATA", db: &APPROVEMENT_THREAD_METADATA},
-		{name: "FINALIZATION_THREAD_METADATA", db: &FINALIZATION_THREAD_METADATA},
+		{name: "BLOCKS", db: BLOCKS},
+		{name: "STATE", db: STATE},
+		{name: "EPOCH_DATA", db: EPOCH_DATA},
+		{name: "APPROVEMENT_THREAD_METADATA", db: APPROVEMENT_THREAD_METADATA},
+		{name: "FINALIZATION_THREAD_METADATA", db: FINALIZATION_THREAD_METADATA},
 	}
 
 	var errs []error
 	for _, database := range databases {
-		if database.db == nil || *database.db == nil {
+		if database.db == nil {
 			continue
 		}
 
-		if err := (*database.db).Close(); err != nil {
+		if err := database.db.Close(); err != nil {
 			errs = append(errs, fmt.Errorf("close %s: %w", database.name, err))
 		}
 	}
